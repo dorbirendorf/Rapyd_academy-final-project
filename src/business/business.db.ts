a45f949d0f148702323444a6007d137c7962480d
Meir Shtarkman <shtarkman.meir@gmail.com>
2022-02-09 15:23:48 +0200

    single select

--- src/business/business.db.ts
+++ src/business/business.db.ts
@@ -1,26 +1,27 @@
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { OkPacket, RowDataPacket } from "mysql2";
-import { createRow, selectRowById, selectRowByIdWithJoin} from "../db.utils.js";
-import { IAddress, IBusiness, IIndividual } from "../types/types.js";
+import { createRow } from "../db.utils.js";
+import { IBusiness, IBusinessFromDb } from "../types/types.js";
+import { db } from "../db/sql/sql.connection.js"
 
-
-export async function createBusinessAccount(business:IBusiness):Promise<number>{
-    const accountRes = await createRow("account",{currency:business.currency,balance:business.balance,status:true, type: "business"})
-    await createRow("business",{account_id:(accountRes as OkPacket).insertId,company_id:business.company_id,company_name:business.company_name,context:business.context,address_id:business.address_id})
+export async function createBusinessAccount(business: IBusiness): Promise<number> {
+    const accountRes = await createRow("account", { currency: business.currency, balance: business.balance, status: true, type: "business" })
+    await createRow("business", { account_id: (accountRes as OkPacket).insertId, company_id: business.company_id, company_name: business.company_name, context: business.context, address_id: business.address_id })
     return (accountRes as OkPacket).insertId;
 }
 
- export async function getBusinessAccountById(accountId:number):Promise<IBusiness>{
-    const accountRes = await selectRowByIdWithJoin("account","business",{primary_id:accountId},"primary_id","account_id");
-    const businessRes = await addAddressToAccount((accountRes as RowDataPacket[])[0] as IBusiness)
-    return businessRes as IBusiness;
+export async function getBusinessAccountById(accountId: number): Promise<IBusiness> {
+    const [rows] = await db.query(`SELECT * FROM business JOIN account on business.account_id = account.primary_id
+    JOIN address on address.address_id = business.address_id WHERE business.account_id = ?`, [accountId])
+    const business = extractBusinessFromObj((rows as RowDataPacket[])[0] as IBusinessFromDb);
+    return business
 }
-
-export async function addAddressToAccount(account:IBusiness|IIndividual):Promise<IBusiness|IIndividual>{
-    const address = await selectRowById("address",{address_id:account.address_id})
-    account.address = address as IAddress;
-    delete account.address_id;
-    return account;
+function extractBusinessFromObj(obj: IBusinessFromDb): IBusiness {
+    const { account_id, currency, balance, amount, status, type, company_id, company_name,
+        context, address_id, country_name, country_code, postal_code, city, region, street_name, street_number } = obj
+    const business: IBusiness = { account_id, currency, balance, amount, status, type, company_id, company_name, context };
+    business.address = { address_id, country_name, country_code, postal_code, city, region, street_name, street_number }
+    return business
 }
 
