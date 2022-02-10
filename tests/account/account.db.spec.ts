e172c42382ae13f954ed1418d19e5695f3fc1dd0
dor birendorf <dorbiren@gmail.com>
2022-02-10 18:20:39 +0200

    feat create signture using cryptojs

--- tests/account/account.db.spec.ts
+++ tests/account/account.db.spec.ts
@@ -8,31 +8,10 @@
 // describe("account db functions ", () => {
 //     context("updateAccountStatus ", () => {
   
-<<<<<<< HEAD
-        it("should be function", () => {
-            expect(updateAccountStatus).to.be.a("Function");
-        });
-        it("should return undefined if ok ", async () => {
-            const res = await updateAccountStatus([2,4,6],true);
-            expect(res).to.be.eqls({fieldCount: 0,
-                affectedRows: 3,
-                insertId: 0,
-                info: 'Rows matched: 3  Changed: 0  Warnings: 0',
-                serverStatus: 2,
-                warningStatus: 0,
-                changedRows: 0}
-              )
-        });
-        // it("should throw error if not ok ", () => {
-        //     expect(()=>updateAccountStatus(2000,1000)).to.throw();
-        // });
-    });
-=======
 //         it("should be function", () => {
 //             expect(updateAccountStatus).to.be.a("Function");
 //         });
 //         it("should return undefined if ok ", async () => {
-        
 //             const res = await updateAccountStatus([2,4,6],true);
 //             expect(res).to.be.eqls({fieldCount: 0,
 //                 affectedRows: 3,
@@ -43,30 +22,29 @@
 //                 changedRows: 0}
 //               )
 //         });
-//         // it("should throw error if not ok ", () => {
-//         //     expect(()=>updateAccountStatus(2000,1000)).to.throw();
-//         // });
+//         it("should throw error if not ok ", () => {
+//             expect(()=>updateAccountStatus(2000,1000)).to.throw();
+//         });
 //     });
->>>>>>> upstream/main
 
 //     context("updateAccountBalance ", () => {
   
 //         it("should be function", () => {
 //             expect(updateAccountBalance).to.be.a("Function");
 //         });
-//         // it("should return undefined if ok ", async() => {
-//         //     const res = await updateAccountBalance([[1,20000],[2,20000],[3,20000],[4,20000],[5,20000]]);
-//         //     expect(res).to.be.eql({fieldCount: 0,
-//         //         affectedRows: 5,
-//         //         insertId: 0,
-//         //         info: 'Rows matched: 5  Changed: 5  Warnings: 0',
-//         //         serverStatus: 2,
-//         //         warningStatus: 0,
-//         //         changedRows: 5})
-//         // });
-//         // it("should throw error if not ok ", () => {
-//         //     expect(()=>updateAccountBalance(7,1234)).to.throw();
-//         // });
+//         it("should return undefined if ok ", async() => {
+//             const res = await updateAccountBalance([[1,20000],[2,20000],[3,20000],[4,20000],[5,20000]]);
+//             expect(res).to.be.eql({fieldCount: 0,
+//                 affectedRows: 5,
+//                 insertId: 0,
+//                 info: 'Rows matched: 5  Changed: 5  Warnings: 0',
+//                 serverStatus: 2,
+//                 warningStatus: 0,
+//                 changedRows: 5})
+//         });
+//         it("should throw error if not ok ", () => {
+//             expect(()=>updateAccountBalance(7,1234)).to.throw();
+//         });
 //     });
 
 //     context("getAccountsById ", () => {
@@ -79,12 +57,12 @@
 //             console.log(accounts);
 //             expect(accounts.length).to.be.equal(5)
 //         });
-//         // it("should throw error if not ok ", () => {
-//         //     expect(()=>getAccountsById(-1000)).to.throw();
-//         // });
-//         // it("should throw error if not ok ", () => {
-//         //     expect(()=>getAccountsById(0)).to.throw();
-//         // });
+//         it("should throw error if not ok ", () => {
+//             expect(()=>getAccountsById(-1000)).to.throw();
+//         });
+//         it("should throw error if not ok ", () => {
+//             expect(()=>getAccountsById(0)).to.throw();
+//         });
 //     });
 
 // });
